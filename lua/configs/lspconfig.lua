94809e1194cc5d963b67f4471f365387aedde3fa
Savva Kodeikin <savva.kodeikin@gmail.com>
2024-05-03 00:03:03 +0200

    fix Python LSP

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,7 @@
 -- EXAMPLE
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
+local root_dir = require("nvchad.configs.lspconfig").root_dir
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
@@ -14,11 +15,16 @@ for _, lsp in ipairs(servers) do
     on_attach = on_attach,
     on_init = on_init,
     capabilities = capabilities,
+    root_dir = root_dir,
   }
 end
 
 -- more on this: https://github.com/younger-1/nvim/blob/one/lua/young/lsp/providers/pyright.lua
 lspconfig.pyright.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+  root_dir = root_dir,
   before_init = function(_, config)
     config.settings.python.pythonPath = get_python_path(vim.fn.getcwd())
   end,
